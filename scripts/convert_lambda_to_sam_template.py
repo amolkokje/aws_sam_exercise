ef9e2150d92444f847d103e2ba1472ca76cdddf4
Amol Kokje <amol_kokje@mcafee.com>
2018-05-17 08:20:48 -0700

    folder structure commit

---
+++ scripts/convert_lambda_to_sam_template.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env
+"""
+This script takes in YAML defining Lambda functions and generate an AWS SAM template as a result.
+"""
---
+++ test/scripts/test_convert_lambda_to_sam_template.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+"""
+This is a test script for convert_lambda_to_template.py. It will take input YAML files from the folder \tests and
+dump the output files in the folder \results.
+"""
