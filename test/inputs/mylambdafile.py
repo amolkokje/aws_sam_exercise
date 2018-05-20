4f421e8663966639c003beddde43926b61a74f4f
Amol Kokje <amol_kokje@mcafee.com>
2018-05-20 16:52:15 -0700

    test files

---
+++ test/inputs/mylambdafile.py
@@ -0,0 +1,8 @@
+def lambda_handler(event, context):
+    print 'event={}'.format(event)
+    print 'context={}'.format(context)
+
+
+def lambda_handler_one(event, context):
+    print 'event_one={}'.format(event)
+    print 'context_one={}'.format(context)
