15e8acce01d9e8bf9c3f491562c60b3f5d801c03
Amol Kokje <amol_kokje@mcafee.com>
2018-05-20 17:15:30 -0700

    need to fix the tests to work. Make this able to run with unittest framework

--- test/scripts/test_convert_lambda_to_sam_template.py
+++ test/scripts/test_convert_lambda_to_sam_template.py
@@ -28,21 +28,33 @@ def test_sam_cli_events_file(template_filepath, event_filepath):
 
 
 def test_convert_lambda_to_sam_template():
-    tests_folder = r'C:\Users\aakokje\Documents\cambia\aws_sam_exercise\test\tests'
-    test_yaml_files = ['test_lambda_list_0.yaml']
-    test_event_files = ['test_event.json']
+    # this will make sure to change path to \inputs directory which contains test inputs
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir(r'..\inputs')
 
+    # get test files
+    test_yaml_files = []
+    test_event_files = []
+    for file in os.listdir(os.getcwd()):
+        if file.endswith('.yaml'):
+            test_yaml_files.append(file)
+        if file.endswith('.json'):
+            test_event_files.append(file)
+    print 'Test yaml files = {}'.format(test_yaml_files)
+    print 'Test event files = {}'.format(test_event_files)
+
+    # iterate all tests
     for file in test_yaml_files:
-        input_filepath = os.path.join(tests_folder, file)
-        assert os.path.exists(input_filepath), 'Input yaml file path {0} does not exist!'.format(input_filepath)
+        input_filepath = os.path.join(os.getcwd(), file)
+        assert os.path.exists(file), 'Input yaml file path {0} does not exist!'.format(file)
         output_filepath = convert_lambda_to_sam_template(input_filepath)
-        print 'Input={0}, Output={1}'.format(input_filepath, output_filepath)
+        print 'Input={0}, Output={1}'.format(file, output_filepath)
 
         test_sam_cli_s3_event('mybucket', 'mybucketprefix', output_filepath)
         for event_file in test_event_files:
-            event_filepath = os.path.join(tests_folder, event_file)
-            assert os.path.exists(event_filepath), 'Input event file path {0} does not exist!'.format(event_filepath)
-            test_sam_cli_events_file(output_filepath, event_filepath)
+            #event_filepath = os.path.join(tests_folder, event_file)
+            assert os.path.exists(event_file), 'Input event file path {0} does not exist!'.format(event_file)
+            test_sam_cli_events_file(output_filepath, event_file)
 
 
 test_convert_lambda_to_sam_template()
